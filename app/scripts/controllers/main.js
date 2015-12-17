cce6758df146593fff1033b77a2361e04de21e6d
achatt89 <admin@abhijitchatterjee.com>
2015-12-17 23:14:35 +0530

    Inital Push

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qualysApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the qualysApp
+ */
+angular.module('qualysApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
