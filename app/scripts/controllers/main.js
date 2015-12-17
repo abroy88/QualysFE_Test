dcc145708ab1030477c497031eb36e57708ce606
achatt89 <admin@abhijitchatterjee.com>
2015-12-18 03:45:33 +0530

    Bonus Point 2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,15 +9,31 @@
  */
 angular.module('qualysApp')
     .controller('MainCtrl', function($scope, $http) {
-
+        $scope.searchText = '';
         $scope.ParentArray = {};
 
         $scope.loadComments = function() {
             $http.get('../json/test.json').success(function(response) {
                 $scope.ParentArray = response;
-                console.log(response);
+                console.log($scope.ParentArray);
             });
 
         };
+        $scope.searchBox = function() {
+            if ($('#search').val().length !== 0) {
+                $('.searchable').each(function() {
+                    //$('.searchable').removeClass('highlight');
+                    var search_value = $("#search").val();
+                    var search_regexp = new RegExp(search_value, "g");
+                    $(this).html($(this).html().replace(search_regexp, "<span class = 'highlight'>" + search_value + "</span>"));
+                });
+            }
+        }
 
+    })
+    .filter('highlight', function($sce) {
+        return function(text, phrase) {
+            if (phrase) text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span class="highlighted">$1</span>');
+            return $sce.trustAsHtml(text)
+        }
     });
